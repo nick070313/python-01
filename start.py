5ff01a6d599d803468cdb9c67a949eab9c36053b
nick070313 <40173758+nick070313@users.noreply.github.com>
2018-06-19 21:34:17 +0800

    Add files via upload

--- start.py
+++ start.py
@@ -10,11 +10,25 @@
 # print(data)
 # data=6**2  #6的2次方
 # print(data)
-n1=input("Enter a Number:")
-n2=input("Enter a Number:")
-n1=int(n1) # int(資料) 將資料轉換成整數型態
+# n1=input("Enter a Number:")
+# n2=input("Enter a Number:")
+# n1=int(n1) # int(資料) 將資料轉換成整數型態
+# n2=int(n2)
+# result=n1+n2
+# print(result)
+#讓使用者輸入5個數字：算總和？哪個數字最大
+n1=input("輸入第1個數字")
+n2=input("輸入第2個數字")
+n3=input("輸入第3個數字")
+n4=input("輸入第4個數字")
+n5=input("輸入第5個數字")
+n1=int(n1)
 n2=int(n2)
-result=n1+n2
-print(result)
-#讓使用者輸入5個數字：算總和？哪個數字最大？
-#使用List列表處理
+n3=int(n3)
+n4=int(n4)
+n5=int(n5)
+a=[n1,n2,n3,n4,n5]
+print(sum(a))
+print(max(a))
+
+

1311e1a97b42f86cd8abaf30cea8ea5a84e086b4
nick070313 <40173758+nick070313@users.noreply.github.com>
2018-06-19 20:20:08 +0800

    Add files via upload

---
+++ start.py
@@ -0,0 +1,20 @@
+# #這是註解，寫給人看的
+# print("Hello Python")
+# print("3.5")
+# input="Hello world"
+# print(input)
+# #運算符號
+# data=3/5
+# print(data)
+# data=100//33
+# print(data)
+# data=6**2  #6的2次方
+# print(data)
+n1=input("Enter a Number:")
+n2=input("Enter a Number:")
+n1=int(n1) # int(資料) 將資料轉換成整數型態
+n2=int(n2)
+result=n1+n2
+print(result)
+#讓使用者輸入5個數字：算總和？哪個數字最大？
+#使用List列表處理
